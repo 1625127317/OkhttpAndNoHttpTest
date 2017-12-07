d6040bcdf874eac701660fdd0dae1b917d043d51
1625127317 <1625127317@qq.com>
2017-12-07 17:09:05 +0800

    初始化控件

--- app/src/main/java/com/example/zsk/okhttpandnohttptest/MainActivity.java
+++ app/src/main/java/com/example/zsk/okhttpandnohttptest/MainActivity.java
@@ -2,13 +2,20 @@ package com.example.zsk.okhttpandnohttptest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button okhttpButton; //测试Okhttp
+    private Button nohttpButton;  //测试NoHttp
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        okhttpButton = (Button) findViewById(R.id.button);
+
+        nohttpButton = (Button) findViewById(R.id.button2);
     }
 }
